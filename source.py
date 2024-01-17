4c85938be997fa197839876a51e3c570b66c73bf
Mick A <90094923+Mikelangelo-A@users.noreply.github.com>
2024-01-17 19:31:00 +0200

    Izveidoju web-scraping funkciju, ieguvu datus.

--- source.py
+++ source.py
@@ -5,22 +5,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import NoSuchElementException
 import time
 
-service=Service()
-option = webdriver.ChromeOptions()
-driver = webdriver.Chrome(service=service, options=option)
-
-
-hasLoggedIn = False
-
-driver.get("https://www.last.fm/")
-driver.implicitly_wait(2)
-
-if(driver.current_url.endswith("/home")):
-    hasLoggedIn = true
-else:
-    driver.get("https://www.last.fm/login")
-    driver.implicitly_wait(5)
-    time.sleep(5)
+def login_with_input() :
+    global choice
     try: 
         cookieBarIsPresent = driver.find_element(By.ID, "onetrust-reject-all-handler")
         cookieBarIsPresent.click()
@@ -29,19 +15,88 @@ else:
         print("No cookies")
         pass
     print("You have not yet logged into your last.fm account!")
-    username = input("Type in your Last.fm username")
-    password = input("Type in your Last.fm password")
+    print("Type in your Last.fm username:")
+    username = input("")
+    print("Type in your Last.fm password:")
+    password = input("")
     driver.find_element("id", "id_username_or_email").send_keys(username)
     driver.find_element("id", "id_password").send_keys(password)
+    while(True):
+        print("What is the type of the recommendations you would like to receive?\n1)Track\n2)Artist\n3)Album")
+        choice = input("")
+        if(not choice.isnumeric()):
+            choice = 0
+            print("Please enter a number")
+        elif(int(choice) < 1 or int(choice) > 3):
+            choice = 0
+            print("Please enter a number between 1 and 3")
+        else: 
+            break
     enterDiv = driver.find_element(By.CLASS_NAME, "form-submit")
     enter = enterDiv.find_element(By.TAG_NAME, "button")
     enter.click()
 
-driver.implicitly_wait(5)
-if(not driver.current_url.endswith("/user/" + username)):
-    print("Something went wrong, please try again.")
-else :
-    driver.get("https://www.last.fm/home")
+def check_login_success(number) :
+    if(not "/user/" in driver.current_url):
+        print("Something went wrong, please try again.")
+        driver.quit()
+    else :
+        if(int(number) == 1):
+            driver.get("https://www.last.fm/home/tracks")
+        elif(int(number) == 2):
+            driver.get("https://www.last.fm/home/artists")
+        elif(int(number) == 3):
+            driver.get("https://www.last.fm/home/albums")
+    hasLoggedIn = True
+
+# def check_element_for_class(element, attribute):
+
+def get_recommended_items(recElement) :
+    dividedInfoList = []
+    for element in recElement:
+        infoList = []
+        try:
+            titleContainer = element.find_element(By.CLASS_NAME, "recs-feed-title").text
+            infoList.append(titleContainer)
+            if(int(choice) == 1 or int(choice) == 3):
+                artistContainer = element.find_element(By.CLASS_NAME, "recs-feed-description")
+                artist = artistContainer.find_element(By.TAG_NAME, "a").text
+                infoList.append(artist)
+            context = element.find_element(By.CLASS_NAME, "context").text
+            infoList.append(context)
+        except NoSuchElementException:
+            print("Error?")
+            pass
+        if(len(infoList) > 0):
+            dividedInfoList.append(infoList)
+    return dividedInfoList
+
+service=Service()
+option = webdriver.ChromeOptions()
+driver = webdriver.Chrome(service=service, options=option)
 
 
-print('Hello')
+hasLoggedIn = False
+choice = 0
+
+driver.get("https://www.last.fm/")
+driver.implicitly_wait(2)
+
+if(driver.current_url.endswith("/home")):
+    hasLoggedIn = true
+else:
+    driver.get("https://www.last.fm/login")
+    time.sleep(6)
+    login_with_input()
+
+time.sleep(5)
+check_login_success(choice)
+print(choice)
+
+recommendationListElement = driver.find_elements(By.CLASS_NAME, "recs-feed-item")
+itemList = get_recommended_items(recommendationListElement)
+
+
+for array in itemList:
+    print(array)
+
