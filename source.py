a34ea1b0e3a00992610cfd97218604d8c5a41312
miksauzins <mauzins16@gmail.com>
2024-01-18 17:52:48 +0200

    papildinātas lietotāja izvēles,csv failu izveide

--- source.py
+++ source.py
@@ -4,15 +4,17 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import NoSuchElementException
 import time
+import os.path
 
 def login_with_input() :
-    global choice
+    global typeChoice
+    global customArtist
     try: 
         cookieBarIsPresent = driver.find_element(By.ID, "onetrust-reject-all-handler")
         cookieBarIsPresent.click()
-        print("found")
+        # print("found")
     except NoSuchElementException:
-        print("No cookies")
+        # print("No cookies")
         pass
     print("You have not yet logged into your last.fm account!")
     print("Type in your Last.fm username:")
@@ -21,35 +23,87 @@ def login_with_input() :
     password = input("")
     driver.find_element("id", "id_username_or_email").send_keys(username)
     driver.find_element("id", "id_password").send_keys(password)
-    while(True):
-        print("What is the type of the recommendations you would like to receive?\n1)Track\n2)Artist\n3)Album")
-        choice = input("")
-        if(not choice.isnumeric()):
-            choice = 0
-            print("Please enter a number")
-        elif(int(choice) < 1 or int(choice) > 3):
-            choice = 0
-            print("Please enter a number between 1 and 3")
-        else: 
-            break
+
     enterDiv = driver.find_element(By.CLASS_NAME, "form-submit")
     enter = enterDiv.find_element(By.TAG_NAME, "button")
     enter.click()
 
-def check_login_success(number) :
+
+def handle_login_route() :
+    global typeChoice
+    global customArtist
+    global recArtist
+    global path
+    recType = "All"
+    if(customArtist):
+        recType = recArtist
     if(not "/user/" in driver.current_url):
         print("Something went wrong, please try again.")
         driver.quit()
     else :
-        if(int(number) == 1):
+        if(int(typeChoice) == 1):
             driver.get("https://www.last.fm/home/tracks")
-        elif(int(number) == 2):
+            path = "./" + recType +"_New_Recommended_Tracks.csv"
+        elif(int(typeChoice) == 2):
             driver.get("https://www.last.fm/home/artists")
-        elif(int(number) == 3):
+            path = "./" + recType + "_New_Recommended_Artists.csv"
+        elif(int(typeChoice) == 3):
             driver.get("https://www.last.fm/home/albums")
+            path = "./"+ recType + "_New_Recommended_Albums.csv"
     hasLoggedIn = True
 
-# def check_element_for_class(element, attribute):
+def handle_rec_items(itemList):
+    global path
+    global typeChoice
+    global customArtist
+    global recArtist
+    fileExists = os.path.isfile(path)
+    with open(path, "a+", encoding="utf-8") as f:
+        if(not fileExists):
+            if(int(typeChoice) == 1):
+                f.write("Song Name,Artist,Context\n")
+            elif(int(typeChoice) == 2):
+                f.write("Artist,Context\n")
+            elif(int(typeChoice) == 3):
+                f.write("Album Name,Artist,Context\n")
+        for element in itemList :
+            if(int(typeChoice) == 1 or int(typeChoice) == 3):
+                if(int(typeChoice) == 1):
+                    elemName = element[0][0:-7].replace(",", ";")
+                else: 
+                    elemName = element[0]
+                elemArtist = element[1]
+                context = element[2].replace(",", ";")
+                fullName = elemName + "," + elemArtist
+                f.seek(0)
+                if (fullName not in f.read()) and ("You have scrobbled" not in context):
+                    if(customArtist):
+                        if(recArtist in context):
+                            print("Added:\n" + elemName + " By " + elemArtist)
+                            f.write(elemName + ",")
+                            f.write(elemArtist + ',')
+                            f.write(context + '\n')
+                    else:
+                        print("Added:\n" + elemName + " By " + elemArtist)
+                        f.write(elemName + ",")
+                        f.write(elemArtist + ',')
+                        f.write(context + '\n')
+            else:
+                elemArtist = element[0]
+                context = element[1].replace(",", ";")
+                f.seek(0)
+                if (elemArtist not in f.read()) and ("You have scrobbled" not in context):
+                    if(customArtist):
+                        if(recArtist in context):
+                            print("Added:\n" + elemArtist)
+                            f.write(elemArtist + ',')
+                            f.write(context + '\n')
+                    else:
+                        print("Added:\n" + elemArtist)
+                        f.write(elemArtist + ',')
+                        f.write(context + '\n')
+
+
 
 def get_recommended_items(recElement) :
     dividedInfoList = []
@@ -58,50 +112,70 @@ def get_recommended_items(recElement) :
         try:
             titleContainer = element.find_element(By.CLASS_NAME, "recs-feed-title").text
             infoList.append(titleContainer)
-            if(int(choice) == 1 or int(choice) == 3):
+            if(int(typeChoice) == 1 or int(typeChoice) == 3):
                 artistContainer = element.find_element(By.CLASS_NAME, "recs-feed-description")
                 artist = artistContainer.find_element(By.TAG_NAME, "a").text
                 infoList.append(artist)
             context = element.find_element(By.CLASS_NAME, "context").text
             infoList.append(context)
         except NoSuchElementException:
-            print("Error?")
+            print("!Ad Element exception Caught!")
             pass
         if(len(infoList) > 0):
             dividedInfoList.append(infoList)
     return dividedInfoList
+
+
+typeChoice = 0
+customArtist = False
+recArtist = ""
+path = ""
+
+while(True):
+    print("What is the type of the recommendations you would like to receive?\n1)Track\n2)Artist\n3)Album")
+    typeChoice = input("")
+    if(not typeChoice.isnumeric()):
+        typeChoice = 0
+        print("Please enter a number")
+    elif(int(typeChoice) < 1 or int(typeChoice) > 3):
+        typeChoice = 0
+        print("Please enter a number between 1 and 3")
+    else: 
+        break
+
+
+recChoice = 0
+while(recChoice == 0):
+    print("Would you like to receive 1)all recommendations 2)recommendations based on specific artist?")
+    recChoice = input("")
+    if(not recChoice.isnumeric()):
+        recChoice = 0
+        print("Please enter a number")
+    elif(int(recChoice) < 1 or int(recChoice) > 2):
+        recChoice = 0
+        print("Please enter a number between 1 and 2")
+    if(int(recChoice) == 2): 
+        customArtist = True
+        print("Please specify(only one) artist to find recommendations for:")
+        recArtist = input()
 
 service=Service()
 option = webdriver.ChromeOptions()
 driver = webdriver.Chrome(service=service, options=option)
 
 
-choice = 0
-
 driver.get("https://www.last.fm/")
-driver.implicitly_wait(2)
+driver.implicitly_wait(1)
 
 if(not driver.current_url.endswith("/home")):
     driver.get("https://www.last.fm/login")
-    time.sleep(6)
+    time.sleep(5)
     login_with_input()
 
-
 time.sleep(5)
-check_login_success(choice)
-print(choice)
+handle_login_route()
 
 recommendationListElement = driver.find_elements(By.CLASS_NAME, "recs-feed-item")
 itemList = get_recommended_items(recommendationListElement)
-
-
-for array in itemList:
-    print(array)
-
-
-## to-do
-## check for existing recommendation txt file
-## make app create txt files for new recommendations
-## ask user for certain artist recommendations to appear
-## only add recommendations if song/artist isn't already in playlist
+handle_rec_items(itemList)
 
